b2f4caace573214896c8f87684dc1fa72a15736b
NikhilMoudgil <nikhilmoudgil799@gmail.com>
2026-09-19 22:30:07 +0530

    server.ts update for deployment

--- src/lib/server.ts
+++ src/lib/server.ts
@@ -5,7 +5,7 @@ export function createClient(request: Request) {
 
   const supabase = createServerClient(
     import.meta.env.VITE_SUPABASE_URL!,
-    import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY!,
+    (import.meta.env.VITE_SUPABASE_ANON_KEY || import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY)!,
     {
       cookies: {
         getAll() {
